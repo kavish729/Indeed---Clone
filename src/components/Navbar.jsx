26b3f8b4a5380e26e2e692d07086a12a292bc307
MUHAMMED FAHIZ P A <fahisayub@gmail.com>
2022-07-19 16:33:00 +0530

    componentfolder

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,13 @@
+import { Container } from '@chakra-ui/react';
+import React from 'react';
+
+const Navbar = () => {
+    return (
+        <Container>
+            
+        </Container>
+        
+    );
+};
+
+export default Navbar;
