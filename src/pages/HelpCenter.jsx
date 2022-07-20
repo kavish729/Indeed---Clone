966fbb61fdf9f5fa22e109ea9db202535e600fa4
Arbaz86 <arbaz10042002@gmail.com>
2022-07-20 17:11:19 +0530

    done thems

--- src/pages/HelpCenter.jsx
+++ src/pages/HelpCenter.jsx
@@ -1,11 +1,13 @@
 import React from "react";
-import { Box } from "@chakra-ui/react";
+import { Box, useColorMode } from "@chakra-ui/react";
 import { NavHelp } from "../components/HelpCenter/NavHelp";
 
 export const HelpCenter = () => {
+  const { colorMode } = useColorMode();
+
   return (
     <Box>
-      <Box p="1rem 1.5rem" bg="#faf9f8">
+      <Box p="1rem 1.5rem" bg={colorMode === "light" ? "#faf9f8" : "#0d171b"}>
         <NavHelp />
       </Box>
     </Box>
