56b6868896db94d906b2c696349ee7a616f66124
Arjun9696 <mallikarjunharalalli@gmail.com>
2022-07-20 20:17:52 +0530

    Navbar under development

---
+++ src/Pages/ComapanyReviews.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const ComapanyReviews = () => {
+  return (
+    <div>ComapanyReviews</div>
+  )
+}
+
+export default ComapanyReviews
---
+++ src/Pages/SalaryGuide.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const SalaryGuide = () => {
+  return (
+    <div>SalaryGuide</div>
+  )
+}
+
+export default SalaryGuide
