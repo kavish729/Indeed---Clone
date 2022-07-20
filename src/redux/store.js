64671bdb8fd049c2ed11f82b6f1bf8620a9a3b82
Arjun9696 <mallikarjunharalalli@gmail.com>
2022-07-20 23:15:10 +0530

    store changes commit

--- src/redux/store.js
+++ src/redux/store.js
@@ -16,8 +16,6 @@ const rootReducer = combineReducers({
 
 const composeEnhancer=window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
-export const store= legacy_createStore(rootReducer,composeEnhancer(applyMiddleware(thunk)));
-
 export const store = legacy_createStore(
   rootReducer,
   composeEnhancer(applyMiddleware(thunk))
