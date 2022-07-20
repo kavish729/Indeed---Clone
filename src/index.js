65536e6f6a8d9922941c32062d0fae7ea060872e
Arjun9696 <mallikarjunharalalli@gmail.com>
2022-07-20 23:17:20 +0530

    conflict resolved  commit

--- src/index.js
+++ src/index.js
@@ -1,19 +1,3 @@
-<<<<<<< HEAD
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
-import { ChakraProvider } from '@chakra-ui/react';
-import { BrowserRouter } from 'react-router-dom';
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <ChakraProvider>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
-  </ChakraProvider>
-=======
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
@@ -39,7 +23,6 @@ root.render(
       </BrowserRouter>
     </ChakraProvider>
   </Provider>
->>>>>>> d98c9b3eba3fe32956f8846eaf72a4bad3f9362a
 );
 
 // If you want to start measuring performance in your app, pass a function
