72822e1eb83dd58e2ed1f9f8ba2189ffb5cfb0df
Kavish Chittora <kavishchittora728@gmail.com>
2022-07-20 20:11:28 +0530

    salaryguide updated

--- src/routes/MainRoutes.jsx
+++ src/routes/MainRoutes.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import { Routes, Route } from "react-router-dom";
 import CompanyReview from "../pages/CompanyReview";
+import SalaryGuide from "../pages/SalaryGuide";
 
 const MainRoutes = () => {
   return (
     <>
       <Routes>
         <Route path="/companyreview" element={<CompanyReview/>} />
+        <Route path="/salaryguide" element={<SalaryGuide/>} />
       </Routes>
     </>
   );
