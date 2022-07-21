c459fcb5fb781c593cf49ebdc6e1f64d9434c88f
Arbaz86 <arbaz10042002@gmail.com>
2022-07-21 17:33:56 +0530

    done article part

--- src/pages/HelpCenter.jsx
+++ src/pages/HelpCenter.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import { Box, useColorMode } from "@chakra-ui/react";
 import { NavHelp } from "../components/HelpCenter/NavHelp";
 import { SearchHelp } from "../components/HelpCenter/SearchHelp";
+import { ArticlesHelp } from "../components/HelpCenter/ArticlesHelp";
 
 export const HelpCenter = () => {
   const { colorMode } = useColorMode();
@@ -11,6 +12,9 @@ export const HelpCenter = () => {
       <Box p="1rem 1.5rem" bg={colorMode === "light" ? "#faf9f8" : "#0d171b"}>
         <NavHelp />
         <SearchHelp />
+      </Box>
+      <Box bg={colorMode === "light" ? "ffffff" : "#040606"}>
+        <ArticlesHelp />
       </Box>
     </Box>
   );
