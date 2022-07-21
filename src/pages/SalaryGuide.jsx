79b5edc11348dfdaf05aa6a56c2303018deeaf16
Kavish Chittora <kavishchittora728@gmail.com>
2022-07-21 21:09:08 +0530

    browse job completed

--- src/pages/SalaryGuide.jsx
+++ src/pages/SalaryGuide.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import "../App.css";
 import Byindustry from "../components/salaryguide/Byindustry";
-
+import Bycompany from "../components/salaryguide/Bycompany";
 const Industryarray = [
   { name: "Elementary School Teacher", salary: "2,47,310 per year" },
   { name: "Human Resources Specialist", salary: "2,15,486 per year" },
@@ -12,8 +12,76 @@ const Industryarray = [
   { name: "Data Entry Clerk", salary: "2,34,715 per year" },
   { name: "Graphic Designer", salary: "2,59,601 per year" },
 ];
+
+const Companyarray = [
+  {
+    name: "Capgimini",
+    rating: "8.8k reviews",
+    compimg:
+      "https://d2q79iu7y748jz.cloudfront.net/s/_squarelogo/128x128/064eff7aab72cb84fbb7f92b82f6ac4a",
+    ratimg:
+      "https://d3hbwax96mbv6t.cloudfront.net/title-webapp/_next/static/images/filled-stars-6d670c620047f09d4cab57ee3ba90cc6.svg",
+  },
+  {
+    name: "Tata Consultancy Services (TCS)",
+    rating: "19.9k reviews",
+    compimg:
+      "https://d2q79iu7y748jz.cloudfront.net/s/_squarelogo/128x128/13b693b4dcc055d2344351b4c9a148e9",
+    ratimg:
+      "https://d3hbwax96mbv6t.cloudfront.net/title-webapp/_next/static/images/filled-stars-6d670c620047f09d4cab57ee3ba90cc6.svg",
+  },
+  {
+    name: "Accenture",
+    rating: "22k reviews",
+    compimg:
+      "https://d2q79iu7y748jz.cloudfront.net/s/_squarelogo/128x128/fff3cc32d7e908a657ea1da5a4a1732b",
+    ratimg:
+      "https://d3hbwax96mbv6t.cloudfront.net/title-webapp/_next/static/images/filled-stars-6d670c620047f09d4cab57ee3ba90cc6.svg",
+  },
+  {
+    name: "Cognizant Technology Solutions",
+    rating: "14.8k reviews",
+    compimg:
+      "https://d2q79iu7y748jz.cloudfront.net/s/_squarelogo/128x128/ce43c82fedc38bf5739f8c90376c3b98",
+    ratimg:
+      "https://d3hbwax96mbv6t.cloudfront.net/title-webapp/_next/static/images/filled-stars-6d670c620047f09d4cab57ee3ba90cc6.svg",
+  },
+  {
+    name: "Byju's",
+    rating: "382 reviews",
+    compimg:
+      "https://d2q79iu7y748jz.cloudfront.net/s/_squarelogo/128x128/349d1c5aed5c3943dadc47139a21b79d",
+    ratimg:
+      "https://d3hbwax96mbv6t.cloudfront.net/title-webapp/_next/static/images/filled-stars-6d670c620047f09d4cab57ee3ba90cc6.svg",
+  },
+  {
+    name: "IBM",
+    rating: "31.5k reviews",
+    compimg:
+      "https://d2q79iu7y748jz.cloudfront.net/s/_squarelogo/128x128/e33d1816f29251d95814fd99eae809df",
+    ratimg:
+      "https://d3hbwax96mbv6t.cloudfront.net/title-webapp/_next/static/images/filled-stars-6d670c620047f09d4cab57ee3ba90cc6.svg",
+  },
+  {
+    name: "Amazon.com",
+    rating: "85.2k reviews",
+    compimg:
+      "https://d2q79iu7y748jz.cloudfront.net/s/_squarelogo/128x128/3e9d43f5c277de169808abdcaf49ba2c",
+    ratimg:
+      "https://d3hbwax96mbv6t.cloudfront.net/title-webapp/_next/static/images/filled-stars-6d670c620047f09d4cab57ee3ba90cc6.svg",
+  },
+  {
+    name: "Indigo Airlines",
+    rating: "410 reviews",
+    compimg:
+      "https://d2q79iu7y748jz.cloudfront.net/s/_squarelogo/128x128/5335229f1b22e90f1e5eafb892704339",
+    ratimg:
+      "https://d3hbwax96mbv6t.cloudfront.net/title-webapp/_next/static/images/filled-stars-6d670c620047f09d4cab57ee3ba90cc6.svg",
+  },
+];
 const SalaryGuide = () => {
   const [Industry, setIndustry] = useState(Industryarray);
+  const [Company, setCompany] = useState(Companyarray);
   return (
     <>
       <div className="salmdiv">
@@ -79,6 +147,8 @@ const SalaryGuide = () => {
         />
       </div>
       {/* // day-3 */}
+
+      {/*  starting Browse top paying jobs by industry */}
       <div style={{ marginLeft: "50px", marginTop: "20px" }}>
         <h1
           style={{ fontSize: "30px", fontWeight: "700", marginBottom: "20px" }}
@@ -116,6 +186,64 @@ const SalaryGuide = () => {
             );
           })}
         </div>
+        {/* ending first mapping part */}
+
+        {/* ending Browse top paying jobs by industry */}
+
+        {/* starting Browse top paying Companies by industry */}
+
+        <div style={{ marginTop: "20px" }}>
+          <h1
+            style={{
+              fontSize: "30px",
+              fontWeight: "700",
+              marginBottom: "20px",
+            }}
+          >
+            Browse top paying Companies by industry
+          </h1>
+          <div>
+            <Bycompany />
+          </div>
+          {/* mapping part */}
+
+          <div className="sacompddiv">
+            {Company.map((item) => {
+              return (
+                <div
+                  style={{
+                    display: "flex",
+                    gap: "20px",
+
+                    lineHeight: "0.5",
+                  }}
+                >
+                  <img style={{ height: "60px" }} src={item.compimg} />
+                  <div style={{ marginTop: "10px" }}>
+                    <p style={{ fontSize: "17px" }}>{item.name}</p>
+                    <span style={{ marginLeft: "280px" }}>{">"}</span>
+                    <div style={{ display: "flex", gap: "8px" }}>
+                      <img
+                        style={{ height: "10px", marginTop: "8px" }}
+                        src={item.ratimg}
+                      />
+                      <p
+                        style={{
+                          marginTop: "8px",
+                          color: "#6f6f6f",
+                          fontSize: "12px",
+                        }}
+                      >
+                        {item.rating}
+                      </p>
+                    </div>
+                  </div>
+                </div>
+              );
+            })}
+          </div>
+        </div>
+        {/* ending Browse top paying companies by industry */}
       </div>
     </>
   );
