42bfe91955c06e6c6a439581f505257c198a5b99
Arbaz86 <arbaz10042002@gmail.com>
2022-07-22 13:31:22 +0530

    change some routes

--- src/components/Routes/AllRouter.jsx
+++ src/components/Routes/AllRouter.jsx
@@ -10,6 +10,7 @@ import Message from "../../pages/Message";
 import Notification from "../../pages/Notification";
 import { HelpCenter } from "../../pages/HelpCenter";
 import EmployersPostJob from "../../pages/EmployersPostJob";
+<<<<<<< Updated upstream
 import EmployerQuestions from "../../pages/EmployerQuestions";
 import CompanyLandingPage from "../../pages/CompanyLandingPage";
 import Snapshot from "../CompanyLandingPage/Tabs/Snapshot";
@@ -20,6 +21,8 @@ import Questions from "../CompanyLandingPage/Tabs/Questions";
 import Interviews from "../CompanyLandingPage/Tabs/Interviews";
 import Photos from "../CompanyLandingPage/Tabs/Photos";
 import Salaries from "../CompanyLandingPage/Tabs/Salaries";
+=======
+>>>>>>> Stashed changes
 const AllRouter = () => {
   return (
     <Routes>
