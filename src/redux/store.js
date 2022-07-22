ec176ffa361753a499df9f0982aacd78eff6c7a5
Arjun9696 <mallikarjunharalalli@gmail.com>
2022-07-22 16:04:43 +0530

    Signup final commit

--- src/redux/store.js
+++ src/redux/store.js
@@ -3,21 +3,16 @@ import {
   combineReducers,
   compose,
   legacy_createStore,
-} from "redux";
-import thunk from "redux-thunk";
-import { appReducer } from "./appReducer/reducer";
+} from 'redux';
+import thunk from 'redux-thunk';
+import { appReducer } from './appReducer/reducer';
+import { reducer as AuthReducer } from './authReducer/reducer';
 
-const rootReducer = combineReducers({
-  appReducer: appReducer,
-});
+const rootReducer = combineReducers({ appReducer, AuthReducer });
 
-
-
-
-const composeEnhancer=window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+const composeEnhancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
 export const store = legacy_createStore(
   rootReducer,
   composeEnhancer(applyMiddleware(thunk))
 );
-
