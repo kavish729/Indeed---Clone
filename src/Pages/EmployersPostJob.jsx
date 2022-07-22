8ef0ba92a1d89db423cae306343ca2d77e6e4683
Arbaz86 <arbaz10042002@gmail.com>
2022-07-22 11:12:00 +0530

    done header part

--- src/Pages/EmployersPostJob.jsx
+++ src/Pages/EmployersPostJob.jsx
@@ -1,9 +1,15 @@
-import React from 'react'
+import { Box } from "@chakra-ui/react";
+import React from "react";
+import HeaderEPJ from "../components/EmployersPostJob/HeaderEPJ";
 
 const EmployersPostJob = () => {
   return (
-    <div>EmployersPostJob</div>
-  )
-}
+    <Box>
+      <Box>
+        <HeaderEPJ />
+      </Box>
+    </Box>
+  );
+};
 
-export default EmployersPostJob
+export default EmployersPostJob;
