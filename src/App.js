a4b5b3b16bed18c7e048d7b43d25ac2fffccc007
MUHAMMED FAHIZ P A <fahisayub@gmail.com>
2022-07-23 18:18:58 +0530

    footer added to pages

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import './App.css';
 import Navbar from './components/Navbar/Navbar'
 import AllRouter from './components/Routes/AllRouter';
-
+import Chakrafooter from './components/salaryguide/Salarymainfooter'
 function App() {
   return (
     <div className="App">
       <Navbar />
       <AllRouter/>
+      <Chakrafooter/>
     </div>
 
   );
