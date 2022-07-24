1b8873c806b2d63c021735797a5d5a4324d32ab8
Arjun9696 <mallikarjunharalalli@gmail.com>
2022-07-24 11:15:46 +0530

    commit

--- src/redux/appReducer/actions.js
+++ src/redux/appReducer/actions.js
@@ -8,42 +8,44 @@ import {
   GET_SINGLE_JOBS_REQUESET,
   GET_SINGLE_JOBS_SUCCESS,
   GET_SINGLE_JOBS_FAILURE,
-} from "./actionType";
+} from './actionType';
 import axios from 'axios';
 
 export const getcompanyApi = () => (dispatch) => {
-    dispatch({type:GET_COMPANY_REQUESET});
-    axios
-    .get('http://localhost:8080/companies')
+  dispatch({ type: GET_COMPANY_REQUESET });
+  axios
+    .get('https://indeeddatabase.herokuapp.com/companies')
     .then((response) => {
-        console.log(response.data);
-        dispatch({type:GET_COMPANY_SUCCESS,payload:response.data});
-      })
-  .catch((error) => {
-      dispatch({type:GET_COMPANY_FAILURE});
-  });
+      console.log(response.data);
+      dispatch({ type: GET_COMPANY_SUCCESS, payload: response.data });
+    })
+    .catch((error) => {
+      dispatch({ type: GET_COMPANY_FAILURE });
+    });
 };
 
 export const getJobs = () => (dispatch) => {
-  dispatch({type:GET_JOBS_REQUESET});
-  axios.get('http://localhost:8080/jobs')
-  .then((response) => {
+  dispatch({ type: GET_JOBS_REQUESET });
+  axios
+    .get('https://indeeddatabase.herokuapp.com/jobs')
+    .then((response) => {
       console.log(response.data);
-      dispatch({type:GET_JOBS_SUCCESS,payload:response.data});
+      dispatch({ type: GET_JOBS_SUCCESS, payload: response.data });
     })
-.catch((error) => {
-    dispatch({type:GET_JOBS_FAILURE});
-});
+    .catch((error) => {
+      dispatch({ type: GET_JOBS_FAILURE });
+    });
 };
 
 export const getSingleJobs = (id) => (dispatch) => {
-  dispatch({type:GET_SINGLE_JOBS_REQUESET});
-  axios.get(`http://localhost:8080/jobs/${id}`)
-  .then((response) => {
+  dispatch({ type: GET_SINGLE_JOBS_REQUESET });
+  axios
+    .get(`https://indeeddatabase.herokuapp.com/jobs/${id}`)
+    .then((response) => {
       console.log(response.data);
-      dispatch({type:GET_SINGLE_JOBS_SUCCESS,payload:response.data});
+      dispatch({ type: GET_SINGLE_JOBS_SUCCESS, payload: response.data });
     })
-.catch((error) => {
-    dispatch({type:GET_SINGLE_JOBS_FAILURE});
-});
+    .catch((error) => {
+      dispatch({ type: GET_SINGLE_JOBS_FAILURE });
+    });
 };
